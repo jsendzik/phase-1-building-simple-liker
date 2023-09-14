567aa7e17b3bbea5e107e8d51b33cbf371dca96c
Jacob Sendzik <jake.sendzik@gmail.com>
2023-09-14 15:38:46 -0400

    Complete

--- main.js
+++ main.js
@@ -3,6 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorModal = document.getElementById("modal");
+errorModal.className = "hidden"
+const hearts = document.getElementsByClassName("like-glyph")
+//console.log(hearts)
+
+function like(e) {
+  if(e.target.innerText == EMPTY_HEART) {
+    e.target.innerText = FULL_HEART;
+    e.target.className = "activated-heart"
+  } else {
+    e.target.innerText = EMPTY_HEART;
+    e.target.className = ""
+  }
+  
+  mimicServerCall()
+    .then(response => alert(response))
+    .catch(error => {
+      errorModal.className = "";
+      errorModal.innerText = error;
+      //console.log(error)
+      setTimeout(() => {
+        errorModal.className = "hidden"
+        }, 3000)
+      }
+    )
+}
+
+for(glyph of hearts) {
+  glyph.addEventListener("click", like)
+}
 
 
 
